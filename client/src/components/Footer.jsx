edcebacdbbf7a98a0a94a58534b5e11b3fd7e0bf
Faizan Sorathiya <ayurlandenterprise43@gmail.com>
2026-04-13 18:24:30 +0530

    All Complete

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -9,6 +9,21 @@ export default function Footer() {
   const isLight = theme === "light";
   const year = new Date().getFullYear();
 
+  const socials = [
+    {
+      icon: FaGithub,
+      link: "https://github.com/Faizyy43",
+    },
+    {
+      icon: FaLinkedin,
+      link: "https://www.linkedin.com/in/faizan-sorathiya-6147a6295?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+    },
+    {
+      icon: Mail,
+      link: "mailto:faizansorthiya0@gmail.com",
+    },
+  ];
+
   return (
     <footer
       className={`relative w-full border-t overflow-hidden
@@ -46,21 +61,27 @@ export default function Footer() {
 
         {/* RIGHT ICONS */}
         <div className="flex items-center gap-3 md:gap-4">
-          {[FaGithub, FaLinkedin, Mail].map((Icon, i) => (
-            <motion.a
-              key={i}
-              href="#"
-              whileHover={{ y: -3, scale: 1.1 }}
-              whileTap={{ scale: 0.9 }}
-              className={`relative p-2 md:p-2.5 rounded-md transition
-              ${isLight ? "hover:bg-gray-100" : "hover:bg-gray-800"}`}
-            >
-              <Icon size={16} />
+          {socials.map((item, i) => {
+            const Icon = item.icon;
 
-              {/* glow effect */}
-              <span className="absolute inset-0 rounded-md opacity-0 hover:opacity-100 transition shadow-[0_0_15px_rgba(236,72,153,0.5)]" />
-            </motion.a>
-          ))}
+            return (
+              <motion.a
+                key={i}
+                href={item.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                whileHover={{ y: -3, scale: 1.1 }}
+                whileTap={{ scale: 0.9 }}
+                className={`relative p-2 md:p-2.5 rounded-md transition
+        ${isLight ? "hover:bg-gray-100" : "hover:bg-gray-800"}`}
+              >
+                <Icon size={16} />
+
+                {/* glow effect */}
+                <span className="absolute inset-0 rounded-md opacity-0 hover:opacity-100 transition shadow-[0_0_15px_rgba(236,72,153,0.5)]" />
+              </motion.a>
+            );
+          })}
         </div>
       </div>
 
